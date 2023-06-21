3941f094d92042270a5c134a8b21763c962a2427
ZaretskiyM <zaretwork@gmail.com>
2023-06-21 22:52:30 +0300

    Homework Testing fix

--- src/test/java/com/heimtn/skyprospringhw/CalculatorServiceImplTest.java
+++ src/test/java/com/heimtn/skyprospringhw/CalculatorServiceImplTest.java
@@ -13,8 +13,88 @@ import java.util.stream.Stream;
 public class CalculatorServiceImplTest {
 
     private final CalculatorService service = new CalculatorServiceImpl();
+    private final int number1 = 2;
+    private final int number2 = 5;
+    private final int number3 = 21;
+    private final int number4 = 385;
+    
+    
+    @Test
+    public void plus(){
+        int actualResult1 = service.plus(number1, number2);
+        int actualResult2 = service.plus(number3, number4);
+        int actualResult3 = service.plus(number1, number3);
+        int actualResult4 = service.plus(number2, number4);
+        
+        int exceptedResult1 = number1 + number2;
+        int exceptedResult2 = number3 + number4;
+        int exceptedResult3 = number1 + number3;
+        int exceptedResult4 = number2 + number4;
 
+        Assertions.assertEquals(exceptedResult1, actualResult1);
+        Assertions.assertEquals(exceptedResult2, actualResult2);
+        Assertions.assertEquals(exceptedResult3, actualResult3);
+        Assertions.assertEquals(exceptedResult4, actualResult4);
+    }
 
+    @Test
+    public void multiply(){
+        int actualResult1 = service.multiply(number1, number2);
+        int actualResult2 = service.multiply(number3, number4);
+        int actualResult3 = service.multiply(number1, number3);
+        int actualResult4 = service.multiply(number2, number4);
+
+        int exceptedResult1 = number1 * number2;
+        int exceptedResult2 = number3 * number4;
+        int exceptedResult3 = number1 * number3;
+        int exceptedResult4 = number2 * number4;
+
+        Assertions.assertEquals(exceptedResult1, actualResult1);
+        Assertions.assertEquals(exceptedResult2, actualResult2);
+        Assertions.assertEquals(exceptedResult3, actualResult3);
+        Assertions.assertEquals(exceptedResult4, actualResult4);
+    }
+
+    @Test
+    public void minus(){
+        int actualResult1 = service.minus(number1, number2);
+        int actualResult2 = service.minus(number3, number4);
+        int actualResult3 = service.minus(number1, number3);
+        int actualResult4 = service.minus(number2, number4);
+
+        int exceptedResult1 = number1 - number2;
+        int exceptedResult2 = number3 - number4;
+        int exceptedResult3 = number1 - number3;
+        int exceptedResult4 = number2 - number4;
+
+        Assertions.assertEquals(exceptedResult1, actualResult1);
+        Assertions.assertEquals(exceptedResult2, actualResult2);
+        Assertions.assertEquals(exceptedResult3, actualResult3);
+        Assertions.assertEquals(exceptedResult4, actualResult4);
+    }
+
+    @Test
+    public void divide(){
+        float actualResult1 = service.divide(number1, number2);
+        float actualResult2 = service.divide(number3, number4);
+        float actualResult3 = service.divide(number1, number3);
+        float actualResult4 = service.divide(number2, number4);
+
+        float exceptedResult1 = (float) number1 / number2;
+        float exceptedResult2 = (float) number3 / number4;
+        float exceptedResult3 = (float) number1 / number3;
+        float exceptedResult4 = (float) number2 / number4;
+
+        Assertions.assertEquals(exceptedResult1, actualResult1);
+        Assertions.assertEquals(exceptedResult2, actualResult2);
+        Assertions.assertEquals(exceptedResult3, actualResult3);
+        Assertions.assertEquals(exceptedResult4, actualResult4);
+    }
+    
+    
+    
+    
+    
     @ParameterizedTest
     @MethodSource("provideParamsForTest")
     public void checkCalculator(int num1, int num2, int expectedResultPlus, int expectedResultMultiply,
